c175beee1bef77c065612ea38e23008072822613
Withered Flowers <withered-flowers@users.noreply.github.com>
2024-02-06 13:52:31 +0700

    feat: finalize content

---
+++ tailwind.config.mjs
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+import defaultTheme from "tailwindcss/defaultTheme";
+
+export default {
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+	theme: {
+		extend: {
+			colors: {
+				primary: "#FDF2F8",
+				secondary: "#F8FDF2",
+				tertiary: "#F2F8FD",
+				"primary-mono": "#F6C6E0",
+				"secondary-mono": "#E0F6C6",
+				"tertiary-mono": "#C6E0F6",
+			},
+			fontFamily: {
+				meow: ["Meow Script", ...defaultTheme.fontFamily.sans],
+				rancho: ["Rancho", ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [],
+};
